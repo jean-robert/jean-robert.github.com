5559f0beb383484abb7365e6d9adb9fb2e17b66d
Jean-Robert <jravettand@gmail.com>
2012-01-31 21:05:10 +0000

    adding somebod hangout

--- Hangout/somebod.js
+++ Hangout/somebod.js
@@ -6,6 +6,10 @@ var offsetTxt = document.getElementById('offsetTxt');
 // Track our overlays for re-use later
 var overlays = [];
 
+// Scale limits---tiny hats look silly, but tiny monocles are fun.
+var minScale = [];
+var maxScale = [];
+
 /** Responds to buttons
  * @param {string} name Item to show.
  */
@@ -14,6 +18,31 @@ function showOverlay(name) {
   currentItem = name;
   setControlVisibility(true);
   overlays[currentItem].setVisible(true);
+  updateControls();
+}
+
+function showNothing() {
+  currentTime = null;
+  hideAllOverlays();
+  setControlVisibility(false);
+}
+
+/** Responds to scale slider
+ * @param {string} value The new scale.
+ */
+function onSetScale(value) {
+  scaleTxt.innerHTML = parseFloat(value).toFixed(2);
+  overlays[currentItem].setScale(parseFloat(value));
+}
+
+/** Responds to offset slider
+ * @param {string} value The new offset.
+ */
+function onSetOffset(value) {
+  console.log('Setting ' + value);
+
+  offsetTxt.innerHTML = parseFloat(value).toFixed(2);
+  overlays[currentItem].setOffset(0, parseFloat(value));
 }
 
 function setControlVisibility(val) {
@@ -22,6 +51,23 @@ function setControlVisibility(val) {
   } else {
     overlayControls.style.visibility = 'hidden';
   }
+}
+
+/** Resets the controls for each type of wearable item */
+function updateControls() {
+  var overlay = overlays[currentItem];
+  var min = minScale[currentItem];
+  var max = maxScale[currentItem];
+
+  document.getElementById('scaleSlider').value = overlay.getScale();
+  document.getElementById('scaleSlider').min = min;
+  document.getElementById('scaleSlider').max = max;
+  document.getElementById('scaleTxt').innerHTML =
+      overlay.getScale().toFixed(2);
+
+  document.getElementById('offsetSlider').value = overlay.getOffset().y;
+  document.getElementById('offsetTxt').innerHTML =
+      overlay.getOffset().y.toFixed(2);
 }
 
 /** For removing every overlay */
@@ -41,6 +87,20 @@ function createOverlays() {
        'scaleWithFace': true,
        'rotateWithFace': true,
        'scale': 1.0});
+  minScale['somebod'] = 0.25;
+  maxScale['somebod'] = 1.5;
+
+  var nobod = gapi.hangout.av.effects.createImageResource(
+      'http://jean-robert.github.com/Hangout/nobod.png');
+  overlays['nobod'] = mono.createFaceTrackingOverlay(
+      {'trackingFeature':
+       gapi.hangout.av.effects.FaceTrackingFeature.NOSE_ROOT,
+       'scaleWithFace': true,
+       'rotateWithFace': true,
+       'scale': 1.0});
+  minScale['nobod'] = 0.25;
+  maxScale['nobod'] = 1.5;
+
 }
 
 createOverlays();
