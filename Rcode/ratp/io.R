b38a6d57b8e6e1212d5941eea78dccba0f7d6386
Jean-Robert <jravettand@gmail.com>
2013-03-22 23:13:27 +0100

    wrong file... no need for io.R

--- Rcode/ratp/io.R
+++
@@ -1,8 +0,0 @@
-shinyUI(bootstrapPage(
-  
-  selectInput(inputId = "friend1",
-              label = "Number of bins in histogram (approximate):",
-              choices = c(T, F),
-              selected = F),
-  
-))
